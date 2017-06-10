adc53b6913766ec921c9bc58aedf4ee133006975
Application support <applicationsupport@semantic15.semantic.com>
2017-06-10 12:38:58 +0530

    Initial Commit

---
+++ sandipDemoforAddDependancyTests/sandipDemoforAddDependancyTests.swift
@@ -0,0 +1,36 @@
+//
+//  sandipDemoforAddDependancyTests.swift
+//  sandipDemoforAddDependancyTests
+//
+//  Created by Application support on 10/06/17.
+//  Copyright © 2017 Sandip Jadhav. All rights reserved.
+//
+
+import XCTest
+@testable import sandipDemoforAddDependancy
+
+class sandipDemoforAddDependancyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
